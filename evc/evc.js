cdf097fd39caf958ef88d4f1a883c64a156bdf2e
Dan Birman <danbirman@gmail.com>
2017-12-22 20:43:23 -0800

    Adding electrodes and starting electrode sprite code

--- evc/evc.js
+++ evc/evc.js
@@ -1,54 +1,81 @@
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
-const app = new PIXI.Application({transparent:true});
+var rendererOptions = {
+  antialiasing: true,
+  transparent: true,
+  resolution: window.devicePixelRatio,
+  autoResize: true,
+}
+
+const app = new PIXI.Application(800,600, rendererOptions);
 
 // The application will create a canvas element for you that you
 // can then insert into the DOM
 document.getElementById("block2").appendChild(app.view);
 
-// var renderer = app.renderer;
-// renderer.view.style.width  = ...... + 'px';
-// renderer.view.style.height = ...... + 'px';
+var brain, e_gray;
+function add_sprites() {
+    brain = PIXI.Sprite.fromImage('images/brain.png');
+    brain.anchor.set(0);
+    brain.scale.set(0.25);
+    brain.x = 0; brain.y = 0;
+    app.stage.addChild(brain);
 
-// load the texture we need
-PIXI.loader.add('bunny', './images/bunny.png').load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
-    const bunny = new PIXI.Sprite(resources.bunny.texture);
+    e_gray = PIXI.Sprite.fromImage('images/electrode_gray.png');
+    // e_red.anchor.set(100,100);
+    e_gray.anchor.set(0,9);
+    e_gray.position.set(300,0);
+    e_gray.scale.set(0.1);
+    e_gray.interactive = true;
+    e_gray
+        .on('pointerdown', buttonDown)
+        .on('pointermove', buttonMove)
+        .on('pointerup', buttonUp)
+        .on('pointerupoutside', buttonUp);
+    app.stage.addChild(e_gray);
+}
 
-    // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
+//////////////////////////////////////////////////////////////////////
+///////////// render loop //////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
+var tick;
 
-    // Add the bunny to the scene we are building
-    app.stage.addChild(bunny);
+function render() {
+    //pass 
+    // tick = requestAnimationFrame(render);
+}
 
-    // Listen for frame updates
-    app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-    });
-});
+//////////////////////////////////////////////////////////////////////
+///////////// interactive functionality //////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
-var brain = PIXI.Sprite.fromImage('images/brain.png');
-brain.anchor.set(0);
-brain.scale = 100;
-brain.x = 0; brain.y = 0;
-app.stage.addChild(brain);
+function buttonDown() {
+    this.isdown = true;
+}
+
+function buttonUp() {
+    this.isdown = false;
+}
+
+function buttonMove(event) {
+    if (this.isdown) {
+        var pos = event.data.getLocalPosition(this.parent);
+        this.position.set(pos.x,pos.y); 
+    }
+}
 
 function run(i) {   
     $("#continue").show();
     switch(i) {
         case 2:
             // pass
+            render();
             break;
     }
 }
 
 function launch_local() {
-    // pass
+    add_sprites();
 }
